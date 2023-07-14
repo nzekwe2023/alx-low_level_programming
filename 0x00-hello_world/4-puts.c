0089832832b94a17643c294171a6e45a60e9d1f7
Nzekwe2023 <chimenzekwe@gmail.com>
2023-07-13 20:02:43 -0700

    up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  *
  *i/
 int main(void)
-{i
-       	puts("\ "Programming is like building a multilingual puzzle");
+{
+       	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
