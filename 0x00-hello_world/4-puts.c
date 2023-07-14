68f638494a1b22cd792125150789c20cc6d82e37
Nzekwe2023 <chimenzekwe@gmail.com>
2023-07-13 17:16:50 -0700

    ok

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main -it prints out output using put
+ *
+ * Return always (0)
+ *
+ */
+int main(void)
+(
+  puts("\"Progressing is like building a multilingual puzzle");
+  return (0);
+)
