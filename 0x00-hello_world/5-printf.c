a81629f008978d53481fccc3c8ce12b72896657a
Nzekwe2023 <chimenzekwe@gmail.com>
2023-07-13 18:51:29 -0700

    gone

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio>
+/**
+ * main -printing output with printf
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	printf("which proper grammar,but the outcome is a place of art,\n");
+	return (0);
+}	
