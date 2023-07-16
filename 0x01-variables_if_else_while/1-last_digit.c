0950b18387984c33772d1daa9ae5cefe5696f2fd
Nzekwe2023 <chimenzekwe@gmail.com>
2023-07-16 16:27:26 -0700

    me

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - prints the last digit of a radomly generated number and whether it is greater than 5,less than 6 or 0
+ *
+ * Return : Always (0)
+ */
+int main(void)
+{
+	int n;
+	int lastd;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+	if (lastd > 5)
+	{
+		printf("last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else
+	{
+		printf(last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+	return (0)
+}		
