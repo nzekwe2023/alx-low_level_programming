6f29cd17d872e685d0983f55d504e6d42b2f782c
Nzekwe2023 <chimenzekwe@gmail.com>
2023-07-19 00:38:34 -0700

    nope

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - genearate random numbers todetect -ve or +ve
  *
@@ -14,15 +14,15 @@ int main(void)
 	n=rand() - RAND_MAX /2;
 	if (n > 0)
 	{
-		printf(%d is positive\n",n);
+		printf("%d is positive\n",n);
 	}
 	else if (n==0)
 	{	
-		printf(%d is zero\n",n);
+		printf("%d is zero\n",n);
 	}
 	else
 	{
-		printf(%d is negative\n",n);
+		printf("%d is negative\n",n);
 	}
 	return (0);
 }	
