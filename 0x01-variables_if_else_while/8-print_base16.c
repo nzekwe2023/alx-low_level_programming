b35d55adda911cdab91463e4fd1d2d4a696554ff
Nzekwe2023 <chimenzekwe@gmail.com>
2023-07-20 15:57:34 -0700

    at last

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 
 /**
- * main _ prints numbers between 0 to 9 and letters between a to f
- *
+ * main - prints numbers between 0 to 9 and letters between a to f
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,12 +10,12 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		putchar (i);
+		putchar(i);
 	}
 	for (i = 97; i < 103; i++)
 	{
-		putchar (i);
+		putchar(i);
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
