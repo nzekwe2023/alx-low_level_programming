e0135c532c66fd897a9951780326f204a3add715
Nzekwe2023 <chimenzekwe@gmail.com>
2023-07-24 01:32:45 -0700

    big

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef main_H
+#define main_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void_puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *n, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
