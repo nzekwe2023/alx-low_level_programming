588b88e0137334274896e91ad535a6f034f0b4de
Nzekwe2023 <chimenzekwe@gmail.com>
2023-07-26 09:20:26 -0700

    mop

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include <unstd.h>
+
+/**
+ * _putchar - writes the chatacter c to stdout
+ * @c: the character to print
+ *
+ * Return: on Success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
