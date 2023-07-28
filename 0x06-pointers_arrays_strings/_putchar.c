814317f9ed3d6b6c77d19400f21b07d681ec679f
Nzekwe2023 <chimenzekwe@gmail.com>
2023-07-28 06:07:08 -0700

    mock

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,14 +1,13 @@
-#include"main.h"
-#include <unstd.h>
-
+#include "main.h"
+#include <unistd.h>
 /**
- * _putchar - writes the chatacter c to stdout
- * @c: the character to print
- *
- * Return: on Success 1.
- * on error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - writes the chatacter c to stdout
+* @c: the character to print
+*
+* Return: on Success 1.
+* on error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
